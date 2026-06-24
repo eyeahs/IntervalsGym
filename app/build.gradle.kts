6b0a9ede4203afd27f39a0db5a3c26a7a50d6b19
hyunwoo park <hyunwoo.pr@navercorp.com>
2026-06-24 16:16:34 +0900

    chore: release 1.3.1
    - refine workout action bars

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.lighthousepark.intervalsgym"
         minSdk = 33
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
