53d54e7acc1d4fab5efe46c55fdceda6732dba90
eyeahs <eyeahs@gmail.com>
2026-07-23 13:30:34 +0900

    chore: release 1.3.27
    - support timed and single-side strength sets

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -236,8 +236,8 @@ android {
         applicationId = "com.lighthousepark.intervalsgym"
         minSdk = 33
         targetSdk = 36
-        versionCode = 30
-        versionName = "1.3.26"
+        versionCode = 31
+        versionName = "1.3.27"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         manifestPlaceholders["intervalsOAuthRedirectScheme"] = intervalsOAuthRedirectScheme
